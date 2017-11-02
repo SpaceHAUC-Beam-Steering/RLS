83ee12dc323a54a0b94f65069833aada4397a220
djd1283 <djd1283@gmail.com>
2017-11-02 08:28:01 -0400

    Upload RLSFinal.cpp
    All matlab functionality converted to C++, except for plotting and filtering. Program is not yet debugged.

--- source/RLSFinal.cpp
+++ source/RLSFinal.cpp
@@ -58,6 +58,7 @@ GenSignalReturn genSignal(double num_points, double frequency, mat filt,
       @param n_var         white noise variance
       @param SNR          signal to noise ratio of tone
     */
+  cout << "genSignal function call" << endl;
 
   cdouble i(0, 1);
 
@@ -67,6 +68,8 @@ GenSignalReturn genSignal(double num_points, double frequency, mat filt,
 
   double elevation = M_PI/3;
   double azimuth = M_PI/3;
+
+  cout << "complex math" << endl;
   
   std::vector<complex<double> > g;
   for(double m = 0; m < 4; m++) {
@@ -84,18 +87,26 @@ GenSignalReturn genSignal(double num_points, double frequency, mat filt,
   }
   cx_rowvec h_arma = cx_rowvec(h);
 
+  cout << "noise calculations" << endl;
+
   // Generate noise
   mat noise = sqrt(n_var)*randn(num_points,1);
   //double addnoise = 1; //filter(filt, 1, noise);
   //freqz(filt, 1, num_points);
-
-  desired = (desired) / sqrt(var(desired)/(pow(10,(SNR/10))*var(noise)));
+  cout << "SNR calculations" << endl;
+  desired = (desired) / sqrt(var(vectorise(desired))/(pow(10,(SNR/10))*var(vectorise(noise))));
   // Display calculated SNR
   cout << "Calculated SNR = " << 5.0 << endl; // num2str(10*log10(var(desired)/var(noise)))])
 
   // Add noise to signal - beware of the real function!
+
+  cx_mat result = desired*g_arma*h_arma.t();
+
+  //cout << result << endl;
   
-  mat received = real(desired*g_arma*h_arma.t()); // + addnoise;
+  mat received = real(result); // + addnoise;
+
+  cout << "genSignal return" << endl;
   
   return GenSignalReturn(received, desired, noise);
 }
@@ -113,7 +124,7 @@ RLSReturn RLS(int lambda, int delta, int sysorder) {
   @error                  a priority estimation error, dim nx1
   @weights                final filter coefficients, dim sysorderx1
   */
-  
+  cout << "RLS function call" << endl;
   int num_points = 1000;
   double frequency = 12e9;
   int filter_order = 16;
@@ -126,6 +137,7 @@ RLSReturn RLS(int lambda, int delta, int sysorder) {
   mat desired = gsr.desired;
   mat noise = gsr.noise;
 
+  cout << "matrices initialize" << endl;
   mat P = eye(sysorder, sysorder) * 1.0/delta;
   //double P = 0; //eye(sysorder)* 1/delta;
   mat weights = zeros<mat>(sysorder,1);
@@ -134,32 +146,49 @@ RLSReturn RLS(int lambda, int delta, int sysorder) {
   double lambda1 = 1/lambda;
   int len = received_vec.n_elem;
 
-  for(int n = sysorder; n < len; n++) {
 
+  for(int n = sysorder; n < len; n++) {
+    
+    //cout << "process input" << endl;
     // Must do indexing manually: input = noise(n:-1:n-sysorder+1);
-    int size = sysorder + 1;
-    mat input = zeros<mat>(size, 1);
-    for(int i = 0; i < n - sysorder; i++) {
+    mat input = zeros<mat>(sysorder, 1);
+    for(int i = n; i < n - sysorder; i++) {
       input(i, 1) = noise(i, 1);
     }
-    
+    input = fliplr(input);
 
-    mat K = (lambda1*P*input)/(1+lambda1*input.t()*P*input); //ahh! there was a '* here somewhere...
+    //cout << "calculate K" << endl;
+
+    mat K = (lambda1*P*input)/as_scalar(1+lambda1*input.t()*P*input); //ahh!
+
+    //cout << "calculate output" << endl;
+    
     mat output = weights.t() * input; // there was another '* here!
-    error(n) = received_vec(n) - output(0);
+    error(n) = received_vec(n) - as_scalar(output);
     weights = weights + K * error(n); //error(n) instead
     P=(lambda1*P)-(lambda1*K*input.t()*P); // another '* here
   }
-  
-  double e = 0; //error(16:1000);
-  double q = -1; //linspace(1, 985, 985);
 
-  double e2 = 0; //abs(e.^2);
+  cout << "calculate error statistics" << endl;
+  
+  mat e = zeros<mat>(1000-16, 1); //error(16:1000);
+  cout << e.size() << endl;
+  cout << error.size() << endl;
+  cout << error(0) << endl;
+  for(int index = 0; index < 1000-16; index++) {
+    double tmp = error(15+index);
+    e(index) = tmp;
+  }
+  cout << "linspace" << endl;
+  mat q = linspace(1, 985, 985);
+
+  mat e2 = abs(pow(e,2));
+
+  cout << "plot" << endl;
 
   // Plot here!
 
   double t = 0; //linspace(0, length(received)/num_points, length(received));
-
   //figure;
   //plot(t, desired, t, error);
   //title('Comparison of Filtered Signal to Original Signal');
@@ -167,15 +196,17 @@ RLSReturn RLS(int lambda, int delta, int sysorder) {
   //plot(q, e2);
 
   // Calculate SNR improvement
-  double SNRi = 10; //10*log10(var(received)/var(error));
+  double SNRi = 10*log10(var(vectorise(received))/var(vectorise(error)));
   cout << SNRi << "dB SNR Improvement" << endl;
+
+  cout << "RLS return" << endl;
 
   return RLSReturn(error, weights);
 }
 
 int main(int argc, char* argv[]) {
   // Driver program.. need parameters to test this functio with.
-  RLS(1,.1,1);
+  RLS(2,.1,10);
   return 0;
 }
 
